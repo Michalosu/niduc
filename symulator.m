dad84cc86dfc154c5ffc3d6e3df5ef61952a9c77
Michalos <michalzdw@gmail.com>
2013-04-17 12:16:26 +0200

    histogram

--- symulator.m
+++ symulator.m
@@ -1,4 +1,3 @@
-t=0;
 nazwa = 'dane.txt';
 [fid, message] = fopen(nazwa,'r');
 if fid==-1
@@ -8,44 +7,77 @@ end
 
 tmax=fscanf(fid,'%d',1);
 liczbaKas=fscanf(fid,'%d',1);
-wsp_przybycia=fscanf(fid,'%d',1);
-wsp_obslugi=fscanf(fid,'%d',1);
+wsp_obslugi=fscanf(fid,'%f',1);
+wybor=fscanf(fid,'%d\n',1);
+wsp_przybycia=fscanf(fid,'%d',10);
+
 fclose(fid);
 
 stan=zeros(1,liczbaKas);
 czas=zeros(1,liczbaKas);
-czasZgloszenia=exprnd(wsp_przybycia);
+klienci=zeros(1,liczbaKas);
+przyjscie=zeros(tmax/2,liczbaKas);
+klienciwyj=zeros(1,liczbaKas);
+wyjscie=zeros(1,liczbaKas);
+czasczekania=zeros(1,liczbaKas);
+klient=0;
+t=0;
+czasZgloszenia=exprnd(wsp_przybycia(1));
 
-nazwa = 'wynik.txt';
+nazwa = 'wynik1.txt';
 [fid, message] = fopen(nazwa,'w');
 if fid==-1
     disp(message)
     return;
 end
 
-while(t<tmax)
-    [a,b,c]=MinCzas(czasZgloszenia, czas, stan, liczbaKas);
-    t=t+c;
-    if(a==1)
-        stan(b)=stan(b)-1;
-        czas(b)=wblrnd(wsp_obslugi,3);
-        czasZgloszenia=czasZgloszenia-c;
-    else
-        [d,e]=min(stan);
-        stan(e)=stan(e)+1;
-        czas=czas-czasZgloszenia;
-        t=t+czasZgloszenia;
-        if(stan(e)==1)
-            czas(e)=wblrnd(wsp_obslugi,3);
+    while(t<tmax)
+
+        [a,b,c]=MinCzas(czasZgloszenia, czas, stan, liczbaKas);
+        t=t+c;
+
+        if(a==1)
+            stan(b)=stan(b)-1;
+            czas(b)=wblrnd(wsp_obslugi,3);
+            czasZgloszenia=czasZgloszenia-c;
+            if(wybor==2)
+                klienciwyj(b)=klienciwyj(b)+1;
+                klient=klient+1;
+                czasczekania(b)=t-przyjscie(klienciwyj(b),b);
+                fprintf(fid,'%d\t', klient);
+                fprintf(fid,'%f\t', czasczekania(b));
+                fprintf(fid,'\r\n');
+            end
+        else
+            [d,e]=min(stan);
+            stan(e)=stan(e)+1;
+            klienci(e)=klienci(e)+1;
+            czas=czas-czasZgloszenia;
+            przyjscie(klienci(e),e)=t;
+
+            if(stan(e)==1)
+                czas(e)=wblrnd(wsp_obslugi,3);
+                % if(wybor==2)
+                %if(klienci(e)==1)
+                %czasczekania(e)=czas(e);
+                %fprintf(fid,'%d\t', klient);
+                %fprintf(fid,'%f\t', czasczekania(e));
+                %fprintf(fid,'\r\n');
+                %end
+                %end
+            end
+            g=ceil(t/(tmax/10));
+            czasZgloszenia=exprnd(wsp_przybycia(g));
         end
-        czasZgloszenia=exprnd(wsp_przybycia);
+        if(wybor==1)
+            fprintf(fid,'%f\t\t',t);
+            for j=1:liczbaKas
+                fprintf(fid,'%d\t', stan(j));
+            end
+            fprintf(fid,'\r\n');
+        end
     end
-
-    fprintf(fid,'%f\t\t',t);
-    for j=1:liczbaKas
-        fprintf(fid,'%d\t', stan(j));
+    if(wybor==2)
+        hist(czasczekania)
     end
-    fprintf(fid,'\r\n');
-
-end
 fclose(fid);
